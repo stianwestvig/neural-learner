83b5150fa88e38f648139c426514c0f81ca3e894
Stian Westvig <stian.westvig@bouvet.no>
2015-02-11 20:52:53 +0100

    test.js a first test of synaptic

---
+++ src/test.js
@@ -0,0 +1,8 @@
+var synaptic = require('synaptic');
+var Neuron = synaptic.Neuron,
+    Layer = synaptic.Layer,
+    Network = synaptic.Network,
+    Trainer = synaptic.Trainer,
+    Achitect = synaptic.Architect;
+
+
